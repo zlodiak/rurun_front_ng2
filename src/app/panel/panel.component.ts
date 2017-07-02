0fb140881734cfc0cf0a98fd27da3facc6eae399
zlodiak <prozaik81-2@yandex.ru>
2017-07-03 00:58:32 +0300

    ajax not worked

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -20,17 +20,15 @@ export class PanelComponent implements OnInit {
       dateFormat: 'dd.mm.yyyy',
   };
 
-  // Initialized to specific date (09.10.2018).
-  /*private modelDateBegin: Object = { date: { year: 2018, month: 10, day: 9 } };  
-  private modelDateEnd: Object = { date: { year: 2018, month: 10, day: 19 } };  */
   private modelDateBegin: Object;  
   private modelDateEnd: Object;  
-  private records: String;  
+  private records: Object;  
+  private data: Object;    
 
   displayStatistic() {
-    console.log(JSON.stringify(this.modelDateBegin['epoc']), JSON.stringify(this.modelDateBegin['epoc']));
-    this.records = this.recordsService.getRecords();
-    console.log(this.records);
+    this.recordsService
+        .getRecords(this.modelDateBegin['epoc'], this.modelDateBegin['epoc'])
+        .subscribe(data => this.records = data.json());
   }
 
 }
--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RecordsService {
 
-  constructor() { }
+  constructor(private http: Http){ }
 
-  getRecords(): String {
-  	return 'okk';
+  getRecords(dateBeginUnix, dateEndUnix): Observable<any> {
+  	console.log(dateBeginUnix, dateEndUnix);
+  	let result = this.http.get('http://127.0.0.1:8000/days/' + dateBeginUnix + '/' + dateEndUnix);
+  	console.log('result', result);
+  	return result;
   } 
 
 }
