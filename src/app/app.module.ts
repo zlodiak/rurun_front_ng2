98dee3e991f84e6f3a92e4d8a63d8821150f7f42
zlodiak <prozaik81-2@yandex.ru>
2017-07-02 12:28:06 +0300

    before datepicker install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
