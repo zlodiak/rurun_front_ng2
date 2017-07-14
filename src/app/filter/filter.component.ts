6b850e70a46eafe2c970ab0c083d71a7efb61883
zlodiak <prozaik81-2@yandex.ru>
2017-07-14 22:39:02 +0300

    fail

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -9,16 +9,34 @@ import { RecordsService } from '../records.service';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
-
-  constructor(private recordsService: RecordsService) { }
-
-  ngOnInit() {
-  }
-
   private modelDateBegin: string;  
   private modelDateEnd: string;  
   private records: Object;  
-  private data: Object;   
+  private data: Object;    
+
+  constructor(private recordsService: RecordsService) { 
+
+  }
+
+  ngOnInit() {
+    let filterValues = sessionStorage.getItem('filterValues');
+
+    if(filterValues) {
+      let filterValuesObj = JSON.parse(filterValues);
+      console.log(filterValuesObj);
+
+      this.modelDateBegin = '' + filterValuesObj.dateBegin.trim();
+      this.modelDateEnd = '' + filterValuesObj.dateEnd.trim();
+      this.limits = filterValuesObj.limits;
+      this.slides = filterValuesObj.slides;
+
+      console.log(typeof this.modelDateBegin, this.modelDateBegin, this.modelDateBegin.length);
+      console.log(typeof this.modelDateEnd, this.modelDateEnd, this.modelDateEnd.length);
+      }
+    
+  }
+
+ 
 
   private slides: Object = {
     'pulse_avg_bpm': undefined,
@@ -37,27 +55,63 @@ export class FilterComponent implements OnInit {
 
   @Output() onSubmitFilter = new EventEmitter<Object>();
 
-  submitFilter() {
+  private submitFilter() {
     let modelDateBeginUnix = Date.parse(this.modelDateBegin)/1000;
     let modelDateEndUnix = Date.parse(this.modelDateEnd)/1000;
+    let modelDateBeginFormatted = this._getFormattedDate(new Date(modelDateBeginUnix * 1000));
+    let modelDateEndFormatted = this._getFormattedDate(new Date(modelDateEndUnix * 1000));    
 
     this.recordsService
         .getRecords(modelDateBeginUnix, modelDateEndUnix)
         .subscribe(data => {
           this.records = JSON.parse(data.json());
+
           this.onSubmitFilter.emit({
           	records: this.records,
           	limits: this.limits
           });
+
+          this.putFilterValuestToSessionStorage(this.limits, 
+                                                modelDateBeginFormatted, 
+                                                modelDateEndFormatted, 
+                                                this.slides);
         });
   }
 
-  toggleSlideVisibility(index) {
+  private toggleSlideVisibility(index) {
     this.slides[index] = !this.slides[index];
   }
 
-  stopPropagator(ev) {
+  private stopPropagator(ev) {
     ev.stopPropagation();
   };    
+
+  private putFilterValuestToSessionStorage(limits, dateBegin, dateEnd, slides) {
+    let obj = {
+      limits: this.limits,
+      dateBegin: dateBegin,
+      dateEnd: dateEnd,
+      slides: slides
+    };
+
+    let objStr = JSON.stringify(obj);
+
+    sessionStorage.setItem('filterValues', objStr);
+  }
+
+  private _getFormattedDate(date) {
+      let dd   = date.getDate();
+      let mm   = date.getMonth() + 1; //January is 0!
+      let yyyy = date.getFullYear();
+
+      if(dd < 10)  { dd = '0' + dd } 
+      if(mm < 10)  { mm = '0' + mm } 
+
+      let result = dd + '.' + mm + '.' + yyyy;
+
+    console.log(date, result)
+
+      return result;
+  }  
 
 }
