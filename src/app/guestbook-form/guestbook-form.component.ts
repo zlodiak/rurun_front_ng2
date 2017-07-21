21f7601f925f8ec61354eab2bd779a11d7f26e1e
zlodiak <prozaik81-2@yandex.ru>
2017-07-21 03:20:20 +0300

    guest form ok

--- src/app/guestbook-form/guestbook-form.component.ts
+++ src/app/guestbook-form/guestbook-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { GuestbookService } from '../services/guestbook.service';
+import { Popup } from 'ng2-opd-popup';
 
 
 @Component({
@@ -13,12 +14,20 @@ export class GuestbookFormComponent implements OnInit {
 	private name: string = '';
 	private message: string = '';
 
+  @ViewChild('popup_ok') popup_ok: Popup;
+  @ViewChild('popup_error') popup_error: Popup;  	
+
   constructor(private guestbookService: GuestbookService) {  }
 
   ngOnInit() {
   }
 
   private submit(): void {
+  	if(!this.name.trim() || !this.message.trim()) {
+  		this.clearFields();
+  		return
+  	};
+
   	this.guestbookService
 	  	.createMessage({
 	  		name: this.name,
@@ -27,9 +36,15 @@ export class GuestbookFormComponent implements OnInit {
 			.subscribe(data => {
         let res = JSON.parse(data.json());
         console.log(res);
+        if(res.status == '1') {
+        	this.showOkModal("Спасибо!");
+        	this.clearFields();
+        } else if(res.status == '0') {
+        	this.showErrorModal(res.error_message);
+        };
       },
     	err => {
-        console.log('err')         
+        //console.log('err')         
       });  
   };
 
@@ -41,6 +56,41 @@ export class GuestbookFormComponent implements OnInit {
   	};
 
   	return result;
+  };
+
+  private showOkModal(headTitle): void {
+    this.popup_ok.show({
+      header: headTitle,
+      color: "blue", // red, blue.... 
+      widthProsentage: 40, // The with of the popou measured by browser width 
+      animationDuration: 1, // in seconds, 0 = no animation 
+      showButtons: true, // You can hide this in case you want to use custom buttons 
+      confirmBtnContent: "OK2", // The text on your confirm button 
+      cancleBtnContent: "OK", // the text on your cancel button 
+      confirmBtnClass: "btn btn-default hide-element", // your class for styling the confirm button 
+      cancleBtnClass: "btn btn-default", // you class for styling the cancel button 
+      animation: "bounceIn" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown' 
+    })  	
+  };
+
+  private showErrorModal(headTitle): void {
+    this.popup_error.show({
+      header: headTitle,
+      color: "red", // red, blue.... 
+      widthProsentage: 40, // The with of the popou measured by browser width 
+      animationDuration: 1, // in seconds, 0 = no animation 
+      showButtons: true, // You can hide this in case you want to use custom buttons 
+      confirmBtnContent: "OK2", // The text on your confirm button 
+      cancleBtnContent: "OK", // the text on your cancel button 
+      confirmBtnClass: "btn btn-default hide-element", // your class for styling the confirm button 
+      cancleBtnClass: "btn btn-default", // you class for styling the cancel button 
+      animation: "bounceIn" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown' 
+    })  	
+  };  
+
+  private clearFields(): void {
+  	this.name = '';
+  	this.message = '';
   };
 
 }
--- src/app/services/guestbook.service.ts
+++ src/app/services/guestbook.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Response, Headers } from '@angular/http';
+import { Response, Headers, URLSearchParams } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import { config } from '../app.config';
@@ -11,11 +11,13 @@ export class GuestbookService {
   constructor(private http: Http){ };
 
   createMessage(obj):Observable<any> {
-    const message = JSON.stringify(obj);
-     
-    let headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
-     
-    return this.http.post(config.host + 'guestbook/message_create', message, { headers: headers });
+		let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }); 
+		let params = new URLSearchParams();
+
+		params.set('name', obj.name.trim());
+		params.set('message', obj.message.trim());
+		
+		return this.http.post(config.host + 'guestbook/message_create', params.toString(), { headers: headers });
   };
 
 }
